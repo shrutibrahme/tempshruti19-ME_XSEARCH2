9295cf9f7d8aa282137002a8987b86115896437a
Atri Biswas <atribiswas3009@gmail.com>
2025-06-11 18:55:55 +0530

    Initial commit

---
+++ src/main/java/apTests/TestCases.java
@@ -0,0 +1,53 @@
+
+package apTests;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.chrome.ChromeDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
+//Selenium Imports
+import java.util.logging.Level;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriverService;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import org.openqa.selenium.logging.LogType;
+import org.openqa.selenium.logging.LoggingPreferences;
+import org.openqa.selenium.remote.BrowserType;
+///
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+public class TestCases {
+    RemoteWebDriver driver;
+
+    public TestCases() throws MalformedURLException {
+        System.out.println("Constructor: TestCases");
+
+        final DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setBrowserName(BrowserType.CHROME);
+        driver = new RemoteWebDriver(new URL("http://localhost:8082/wd/hub"), capabilities);
+
+
+        // Set browser to maximize and wait
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+
+    }
+
+    public void endTest() {
+        System.out.println("End Test: TestCases");
+        driver.close();
+        driver.quit();
+
+    }
+
+    public void testCase01() {
+        System.out.println("Start Test case: testCase01");
+        driver.get("https://www.google.com");
+        System.out.println("end Test case: testCase01");
+    }
+
+}
+
